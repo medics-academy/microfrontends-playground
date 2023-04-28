d6f4090c1692439553dcc669d7bdb49f78bf09e2
emmemorandini <emmemorandini@gmail.com>
2023-04-28 11:24:22 +0100

    Shell: Created core module and home component Configured routes Configured webpack.config.js

--- shell/src/app/app-routing.module.ts
+++ shell/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './core/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('mf1/Module').then(m => m.HeroesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
